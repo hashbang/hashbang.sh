513bd92aef4215336b81108ae01f56a1bb116f50
The Fox in the Shell <KellerFuchs@hashbang.sh>
2016-06-12 22:08:31 +0200

    Make PEP8-clean

--- server.py
+++ server.py
@@ -3,10 +3,8 @@
 import os
 import sys
 import traceback
-import csv
 import ldap
 import ssl
-from subprocess import check_call, CalledProcessError
 from flask import Flask, send_from_directory, redirect, request, make_response
 from flask.ext.restful import Resource, Api
 from flask.ext.restful.reqparse import RequestParser
@@ -25,7 +23,7 @@ api = Api(app)
 
 p = Provisor(
     uri="ldap://ldap.hashbang.sh",
-    user ="cn=provisor,ou=Admin,dc=hashbang,dc=sh",
+    user="cn=provisor,ou=Admin,dc=hashbang,dc=sh",
     password=os.environ['LDAP_PASSWORD'],
     user_base="ou=People,dc=hashbang,dc=sh",
     group_base="ou=Group,dc=hashbang,dc=sh",
@@ -37,18 +35,20 @@ keyfile = os.path.join(os.getcwd(), "certs/server.key")
 https_port = 4443
 http_port = 8080
 
+
 @api.representation('text/plain')
 def output_plain(data, code, headers=None):
-    lines=[]
-    for server,stats in data.items():
+    lines = []
+    for server, stats in data.items():
         line = [server]
-        for key,val in stats.items():
+        for key, val in stats.items():
             line.append(str(val))
         lines.append("|".join(line))
     data = "\n".join(lines)
     resp = make_response(data, code)
     resp.headers.extend(headers or {})
     return resp
+
 
 class UserCreate(Resource):
     def __init__(self):
@@ -84,27 +84,28 @@ class UserCreate(Resource):
                 hostname=args['host']
             )
         except ldap.SERVER_DOWN:
-            return { 'message': 'Unable to connect to LDAP server'}, 400
+            return {'message': 'Unable to connect to LDAP server'}, 400
         except ldap.ALREADY_EXISTS:
-            return { 'message': 'User already exists'}, 400
+            return {'message': 'User already exists'}, 400
         except UNKNOWN_HOST:
-            return { 'message': 'Unknown shell server' }, 400
+            return {'message': 'Unknown shell server'}, 400
         except:
             (typ, value, tb) = sys.exc_info()
             sys.stderr.write("Unexpected Error: %s\n" % typ)
             sys.stderr.write("\t%s\n" % value)
             traceback.print_tb(tb)
-            return { 'message': 'User creation script failed'}, 400
+            return {'message': 'User creation script failed'}, 400
 
         return {'message': 'success'}
 
+
 class ServerStats(Resource):
 
-    def get(self,out_format='json'):
+    def get(self, out_format='json'):
         try:
             server_stats = p.server_stats()
         except ldap.SERVER_DOWN:
-            return { 'message': 'Unable to connect to LDAP server'}, 400
+            return {'message': 'Unable to connect to LDAP server'}, 400
 
         return server_stats
 
@@ -112,27 +113,28 @@ class ServerStats(Resource):
 api.add_resource(UserCreate, '/user/create')
 api.add_resource(ServerStats, '/server/stats')
 
-@app.route('/',methods=["GET"])
+
+@app.route('/', methods=["GET"])
 def root():
     useragent = request.headers.get('User-Agent')
     if 'curl' in useragent and not request.is_secure:
-        return send_from_directory('static','warn.sh')
+        return send_from_directory('static', 'warn.sh')
     if not os.path.isfile(certfile) and not os.path.isfile(keyfile):
-        return send_from_directory('static','index.html')
+        return send_from_directory('static', 'index.html')
     if request.is_secure:
-        return send_from_directory('static','index.html')
+        return send_from_directory('static', 'index.html')
     return redirect(request.url.replace("http://", "https://"))
 
 if __name__ == '__main__':
 
     if os.path.isfile(certfile) and os.path.isfile(keyfile):
         https_server = HTTPServer(
-          WSGIContainer(app),
-          ssl_options={
-              "certfile": certfile,
-              "keyfile": keyfile,
-              "ssl_version": ssl.PROTOCOL_TLSv1
-          }
+            WSGIContainer(app),
+            ssl_options={
+                "certfile": certfile,
+                "keyfile": keyfile,
+                "ssl_version": ssl.PROTOCOL_TLSv1
+            }
         )
         https_server.listen(https_port)
 
