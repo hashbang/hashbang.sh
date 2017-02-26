2d36a358173d2f6bd76e02c33158be837e943e50
The Fox in the Shell <KellerFuchs@hashbang.sh>
2017-02-25 19:37:37 -0500

    Move icon from scripts.js to HTML

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,7 @@
-
 window.onload = function(){
     console.log("loaded")
     window.location="#!"
-    var link = document.createElement("link");
-    link.type = "image/png";
-    link.rel = "icon";
-    link.href = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQAAAAA3iMLMAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAd2KE6QAAAAYSURBVAjXY2CAAck+EKp/B0II9gMoGwYA4+MJkeae/NUAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTQtMDUtMTdUMTM6MzI6MTMtMDQ6MDB7pieOAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE0LTA1LTE3VDEzOjMxOjQ4LTA0OjAwqyt+rwAAAABJRU5ErkJggg==";
-    document.getElementsByTagName("head")[0].appendChild(link);
+
     var sourceEl = document.getElementById("view-source")
     sourceEl.removeAttribute("href");
     sourceEl.onclick = function(){
